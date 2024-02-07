e103f0d41147256fb78f557b1c863e0080e41e71
Cesar Faber <cesarolvr@Cesars-MacBook-Air.local>
2024-02-06 22:57:25 -0300

    adding manisfest

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,40 @@
 module.exports = {
   siteMetadata: {
     title: `cesarolvr`,
-    siteUrl: `https://www.cesarolvr.com`
+    siteUrl: `https://www.cesarolvr.com`,
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "cesarolvr.com",
+        short_name: "cesarolvr",
+        start_url: "/",
+        background_color: "#222222",
+        theme_color: "#222222",
+        display: "standalone",
+        icon: "src/images/icon.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
+  ],
 };
