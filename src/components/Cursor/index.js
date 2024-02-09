a5c503d91ccdcd09ede88d20c956e5180af3b3b8
Cesar Faber <cesarolvr@Cesars-MacBook-Air.local>
2024-02-08 21:32:59 -0300

    modal working with keyboard navigation

--- src/components/Cursor/index.js
+++ src/components/Cursor/index.js
@@ -17,12 +17,9 @@ const Cursor = () => {
         endY: window.innerHeight / 2,
         cursorVisible: true,
         cursorEnlarged: false,
-        $dot: document.querySelector(".ppk-dot"),
         $outline: document.querySelector(".ppk-dot-outline"),
 
         init: function () {
-          // Set up element sizes
-          this.dotSize = this.$dot.offsetWidth;
           this.outlineSize = this.$outline.offsetWidth;
 
           this.setupEventListeners();
@@ -32,7 +29,6 @@ const Cursor = () => {
         setupEventListeners: function () {
           var self = this;
 
-          // Anchor hovering
           document.querySelectorAll("a, button").forEach(function (el) {
             el.addEventListener("mouseover", function () {
               self.cursorEnlarged = true;
@@ -44,7 +40,6 @@ const Cursor = () => {
             });
           });
 
-          // Click events
           document.addEventListener("mousedown", function () {
             self.cursorEnlarged = true;
             self.toggleCursorSize();
@@ -55,29 +50,22 @@ const Cursor = () => {
           });
 
           document.addEventListener("mousemove", function (e) {
-            // Show the cursor
             self.cursorVisible = true;
             self.toggleCursorVisibility();
 
-            // Position the dot
             self.endX = e.pageX;
             self.endY = e.pageY;
-            self.$dot.style.top = self.endY + "px";
-            self.$dot.style.left = self.endX + "px";
           });
 
-          // Hide/show cursor
           document.addEventListener("mouseenter", function (e) {
             self.cursorVisible = true;
             self.toggleCursorVisibility();
-            self.$dot.style.opacity = 1;
             self.$outline.style.opacity = 1;
           });
 
           document.addEventListener("mouseleave", function (e) {
             self.cursorVisible = true;
             self.toggleCursorVisibility();
-            self.$dot.style.opacity = 0;
             self.$outline.style.opacity = 0;
           });
         },
@@ -97,11 +85,9 @@ const Cursor = () => {
           var self = this;
 
           if (self.cursorEnlarged) {
-            self.$dot.style.transform = "translate(-50%, -50%) scale(0.75)";
             self.$outline.style.transform = "translate(-50%, -50%) scale(2)";
             self.$outline.style.borderColor = "white";
           } else {
-            self.$dot.style.transform = "translate(-50%, -50%) scale(1)";
             self.$outline.style.transform = "translate(-50%, -50%) scale(1)";
             self.$outline.style.borderColor = "#3a3a3a";
           }
@@ -111,10 +97,8 @@ const Cursor = () => {
           var self = this;
 
           if (self.cursorVisible) {
-            self.$dot.style.opacity = 1;
             self.$outline.style.opacity = 1;
           } else {
-            self.$dot.style.opacity = 0;
             self.$outline.style.opacity = 0;
           }
         },
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -82,6 +82,7 @@ const actionList = [
 
 const Modal = () => {
   const [list, setList] = React.useState(actionList);
+  const [cursor, setCursor] = React.useState(0);
 
   const { modalIsOpened, setModalIsOpened, setCopied } =
     React.useContext(State);
@@ -89,7 +90,6 @@ const Modal = () => {
   const copyLink = (link) => {
     navigator.clipboard.writeText(link).then(() => {
       setCopied(true);
-      console.log("copied");
       setTimeout(() => {
         setCopied(false);
       }, 1000);
@@ -100,23 +100,73 @@ const Modal = () => {
     Mousetrap.bind(["command+enter", "ctrl+enter"], (e) => {
       e.preventDefault();
       setModalIsOpened(!modalIsOpened);
+      setCursor(0);
     });
 
     Mousetrap.bind("esc", (e) => {
       e.preventDefault();
       setModalIsOpened(false);
+      setCursor(0);
     });
 
     const isDesktop =
       typeof window !== "undefined" ? window.innerWidth > 730 : true;
 
-    if (modalIsOpened && isDesktop) document.querySelector("#shortcutid").focus();
-  }, [modalIsOpened]);
+    if (modalIsOpened && isDesktop) {
+      document.querySelector("#shortcutid").focus();
+
+      Mousetrap.bind("down", (e) => {
+        e.preventDefault();
+        onDown();
+      });
+
+      Mousetrap.bind("up", (e) => {
+        e.preventDefault();
+        onUp();
+      });
+
+      Mousetrap.bind("enter", (e) => {
+        e.preventDefault();
+        if (cursor === 0) {
+          return copyLink(list[0].textToCopy);
+        } else {
+          const link = document.querySelector(`#tablist-${cursor} a`);
+          if (link) return window.open(link.href, "_blank");
+        }
+      });
+    } else {
+      Mousetrap.unbind(["down", "up", "enter"]);
+    }
+  }, [modalIsOpened, cursor, list]);
 
   const onSearch = (e) => {
     const value = e?.target?.value;
     const newList = actionList.filter((item) => item?.text?.includes(value));
     setList(newList);
+    setCursor(0);
+  };
+
+  const onUp = () => {
+    if (list.length === 1) return setCursor(0);
+    if (cursor === 0) return;
+
+    const currentItem = document.querySelector(`#tablist-${cursor}`);
+
+    if (currentItem) {
+      currentItem.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+    setCursor(cursor - 1);
+  };
+  const onDown = () => {
+    if (list.length === 1) return setCursor(0);
+    if (cursor === list.length - 1) return;
+
+    const currentItem = document.querySelector(`#tablist-${cursor}`);
+
+    if (currentItem) {
+      currentItem.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+    setCursor(cursor + 1);
   };
 
   return (
@@ -129,6 +179,7 @@ const Modal = () => {
         className="layer"
         onClick={() => {
           setModalIsOpened(false);
+          setCursor(0);
         }}
       ></div>
       <div className="panel">
@@ -148,18 +199,34 @@ const Modal = () => {
                 return (
                   <li
                     role="option"
+                    id={`tablist-${index}`}
+                    tabIndex={0}
                     key={index}
                     onClick={() => copyLink(textToCopy)}
+                    className={classNames({
+                      "-active": cursor === index,
+                    })}
                   >
-                    <div className="description">
+                    <a
+                      className="description"
+                      href=""
+                      onClick={(e) => e.preventDefault()}
+                    >
                       {icon}
                       <p>{text}</p>
-                    </div>
+                    </a>
                   </li>
                 );
               } else {
                 return (
-                  <li role="option" key={index}>
+                  <li
+                    role="option"
+                    key={index}
+                    id={`tablist-${index}`}
+                    className={classNames({
+                      "-active": cursor === index,
+                    })}
+                  >
                     <a href={target} className="description" target="_blank">
                       {icon}
                       <p>{text}</p>
